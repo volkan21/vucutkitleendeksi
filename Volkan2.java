2395ee49865363b2e77e008966e5c98cf85813d1
volkan21 <volkanerdogus@gmail.com>
2019-06-18 23:06:19 +0300

    Add files via upload

---
+++ Volkan2.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package volkan2;
+import java.sql.*;
+import com.mysql.jdbc.Driver;
+import java.util.Scanner;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.FilterWriter;
+import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.concurrent.ForkJoinPool;
+
+
+
+/**
+ *
+ * @author volkan
+ */
+public class Volkan2 { 
+
+    /**
+     * @param args the command line arguments
+     */
+    
+//    static String username="root";
+//    static String password="lpsycho21";
+//    static String dburl="jdbc:mysql://localhost:3306/world";
+    public static void main(String[] args)  {
+     
+}
+}
+
+
+            
+
+
+       
+     
+        
+       
+        
+
+     
+//     
+    
+
+           
+       
+       
+      
+       
+   
+
+    
+
+    
+  
+
+
+   
+   
+        
+        
+        
+        
+        
+        
+       
+        
+       
+        
+
+
+       
+
+
